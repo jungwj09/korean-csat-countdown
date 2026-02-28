b4cad3094b4754078e6053674d58b4834685462c
jungwj09 <woox1jin@gmail.com>
2026-02-28 19:24:44 +0900

    fix: update footer message

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -3,7 +3,7 @@ import "./Copyright.css";
 export default function Copyright() {
   return (
     <footer className="copyright">
-      <p className="copyright-message">이 시간을 버티는 당신을 응원합니다🍀</p>
+      <p className="copyright-message">끝까지 포기하지 않는 당신을 응원합니다🍀</p>
       <p className="copyright-legal">ⓒ 2026. <a href="https://github.com/jungwj09/09-korean-csat-countdown">09-korean-csat-countdown.</a> All rights reserved.</p>
     </footer>
   );
