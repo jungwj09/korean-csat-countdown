8a581979886cbabf33b086b014837514288ed0c3
jungwj09 <woox1jin@gmail.com>
2026-03-01 13:41:07 +0900

    chore: update footer copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -4,7 +4,7 @@ export default function Copyright() {
   return (
     <footer className="copyright">
       <p className="copyright-message">끝까지 포기하지 않는 당신을 응원합니다🍀</p>
-      <p className="copyright-legal">ⓒ 2026. <a href="https://github.com/jungwj09/09-korean-csat-countdown">09-korean-csat-countdown.</a> All rights reserved.</p>
+      <p className="copyright-legal">ⓒ 2026. <a href="https://github.com/jungwj09/korean-csat-countdown">korean-csat-countdown.</a> All rights reserved.</p>
     </footer>
   );
 }
