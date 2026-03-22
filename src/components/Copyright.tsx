ad545a06b156da3c71cc9196b551bb150ddb5d90
jungwj09 <woox1jin@gmail.com>
2026-03-22 16:13:35 +0900

    feat: add developer

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -5,6 +5,7 @@ export default function Copyright() {
     <footer className="copyright">
       <p className="copyright-message">끝까지 포기하지 않는 당신을 응원합니다🍀</p>
       <p className="copyright-legal">ⓒ 2026. <a href="https://github.com/jungwj09/korean-csat-countdown">korean-csat-countdown.</a> All rights reserved.</p>
+      <p className="copyright-developer">Developed by <a href="https://github.com/jungwj09">jungwj09</a></p>
     </footer>
   );
 }
